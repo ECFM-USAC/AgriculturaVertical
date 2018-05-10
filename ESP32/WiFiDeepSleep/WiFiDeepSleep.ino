50cfb1eac1c94d9a9dc8c3e353c6d3de479ef558
imoralesgt <ivan.rene.morales@gmail.com>
2018-05-10 13:01:05 -0600

    Removed unsed code

--- ESP32/WiFiDeepSleep/WiFiDeepSleep.ino
+++ ESP32/WiFiDeepSleep/WiFiDeepSleep.ino
@@ -46,30 +46,6 @@ void setup()
 
     const uint16_t port = 1883;
     const char * host = "192.168.126.119"; // ip or dns
-/*
-    Serial.print("connecting to ");
-    Serial.println(host);
-
-    // Use WiFiClient class to create TCP connections
-    WiFiClient client;
-
-    if (!client.connect(host, port)) {
-        Serial.println("connection failed");
-        Serial.println("wait 5 sec...");
-        delay(5000);
-        return;
-    }
-
-    // This will send the request to the server
-    client.print("Send this data to server");
-
-    //read back one line from server
-    String line = client.readStringUntil('\r');
-    client.println(line);
-
-    Serial.println("closing connection");
-    client.stop();
-*/    
 
     digitalWrite(LED_BUILTIN, 0);  
 
